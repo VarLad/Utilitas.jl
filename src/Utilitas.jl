120d43dc0067235b7daa75f550db54912566ef3e
Chetan Vardhan <51269425+VarLad@users.noreply.github.com>
2021-05-06 22:30:30 +0530

    Update Utilitas.jl

--- src/Utilitas.jl
+++ src/Utilitas.jl
@@ -6,6 +6,11 @@ module Utilitas
 	using JLFzf: inter_fzf
 	using JSON3: read
 	
+	function __init__()
+		reglist = parse(see\kstart(download("https://raw.githubusercontent.com/JuliaRegistries/General/master/Registry.toml", IOBuffer())))["packages"]
+		k = [c for (b,c) in [a for (a, b) in values(reglist)]]
+	end	
+
 	export search, seadd, gelat, chklat, chkall
 	
 	include("pkgops.jl")
